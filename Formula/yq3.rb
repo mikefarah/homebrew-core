29ffccfa31cfb2fa63f7319a9db4fab31c7c590e
Mike Farah <mikefarah@gmail.com>
2020-12-29 11:19:43 +1100

    yq3 - installs v3 of yq

---
+++ Formula/yq3.rb
@@ -0,0 +1,31 @@
+class Yq3 < Formula
+  desc "Process YAML documents from the CLI - Version 3"
+  homepage "https://github.com/mikefarah/yq"
+  url "https://github.com/mikefarah/yq/archive/3.4.1.tar.gz"
+  sha256 "73259f808d589d11ea7a18e4cd38a2e98b518a6c2c178d1ec57d9c5942277cb1"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  conflicts_with "python-yq", because: "both install `yq` executables"
+  conflicts_with "yq", because: "yq is v4, installs `yq` executable"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/mikefarah/yq").install buildpath.children
+
+    cd "src/github.com/mikefarah/yq" do
+      system "go", "build", "-o", bin/"yq"
+      prefix.install_metafiles
+
+      (bash_completion/"yq").write Utils.safe_popen_read("#{bin}/yq", "shell-completion", "-V=bash")
+      (zsh_completion/"_yq").write Utils.safe_popen_read("#{bin}/yq", "shell-completion", "-V=zsh")
+      (fish_completion/"yq.fish").write Utils.safe_popen_read("#{bin}/yq", "shell-completion", "-V=fish")
+    end
+  end
+
+  test do
+    assert_equal "key: cat", shell_output("#{bin}/yq n key cat").chomp
+    assert_equal "cat", pipe_output("#{bin}/yq r - key", "key: cat", 0).chomp
+  end
+end
